54f3258aa17c5f50a704e9a6ff0bbced784c6aee
wang.shaohui <wang.shaohui@navercorp.com>
2022-10-26 13:17:49 +0800

    save

---
+++ TestBoostMemoryPool/TestBoostMemoryPool.h
@@ -0,0 +1,32 @@
+﻿
+// TestBoostMemoryPool.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CTestBoostMemoryPoolApp:
+// 有关此类的实现，请参阅 TestBoostMemoryPool.cpp
+//
+
+class CTestBoostMemoryPoolApp : public CWinApp
+{
+public:
+	CTestBoostMemoryPoolApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTestBoostMemoryPoolApp theApp;
---
+++ TestBoostMemoryPool/TestPool.cpp
@@ -0,0 +1,48 @@
+﻿#include "pch.h"
+#include "TestBoostMemoryPoolDlg.h"
+#include <iostream>
+#include <Windows.h>
+#include <assert.h>
+#include <memory>
+
+#include <boost/pool/pool.hpp>
+using namespace boost;
+
+const auto ONE_MB = (1024 * 1024);
+
+void TestSimplePool()
+{
+	// 使用默认分配器，其内部使用new[]、delete[]分配内存
+	// 注意：这个pool对象释放的时候 会释放所有通过其申请的堆内存
+	std::shared_ptr<pool<>> testMemPool = std::make_shared<pool<>>(ONE_MB);
+
+	char *p = static_cast<char *>(testMemPool->malloc());
+	assert(testMemPool->is_from(p));
+	testMemPool->free(p);
+
+	int *p2 = new int;
+	assert(testMemPool->is_from(p2) == false);
+	delete p2;
+
+	for (int i = 0; i < 1024; ++i) {
+		void *ret = testMemPool->malloc();
+		printf("ret: %p \n", ret);
+		Sleep(50);
+	}
+
+	OutputDebugStringA("func to end, press any key to free all memory \n");
+	Sleep(5000);
+	// 函数结束时 pool对象会析构 其申请的所有堆内存 都会被释放
+}
+
+void LoopTestSimplePool()
+{
+	for (int i = 0; i < 100; ++i) {
+		TestSimplePool();
+	}
+}
+
+void CTestBoostMemoryPoolDlg::TestSimplePool()
+{
+	// TODO
+}
